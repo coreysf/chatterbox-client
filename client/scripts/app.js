64ef72674d69d5a45b2c079587694ecd54063960
Hack Reactor Students <accounts@hackreactor.com>
2015-08-31 21:05:21 -0700

    Add complete room selector and draft friend functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,6 +4,7 @@
 var displayedMessages = {};
 var messageProperties = {};
 var roomsList = {};
+var friendsList = {};
 
 $(document).ready(function(){
 
@@ -29,16 +30,20 @@ $(document).ready(function(){
               $("#roomSelector").append($roomNode);
             }
             displayedMessages[msgObject.objectId] = msgObject;
-            var node = $("<div></div>");
-            node.addClass("chat");
-            node.attr("id", msgObject.objectId);
-            node.text("User " + msgObject.username + 
-              " message: " + msgObject.text + " created: "
+            var $node = $("<div></div>");
+            var $userLink = $("<span></span>");
+            $userLink.addClass("user");
+            $userLink.text(msgObject.username);
+            $node.addClass("chat");
+            $node.attr("id", msgObject.objectId);
+            $node.text( " message: " + msgObject.text + " created: "
                + msgObject.createdAt
                + " room " + msgObject.roomname);
-            $("#chats").prepend(node);
+            $node.prepend($userLink);
+            $("#chats").prepend($node);
          }
         });
+        changeRoom();
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -54,8 +59,8 @@ $(document).ready(function(){
     var $input = $('#sendMessage :input');
     var message = {
       username: (prompt('What is your name?') || 'anonymous'),
-      text: $input.val(),
-      roomname:"room"
+      text: $($input[0]).val(),
+      roomname: $($input[1]).val()
     };
 
     // alert($input.val());
@@ -79,11 +84,22 @@ $(document).ready(function(){
       }
     });
   });
-  function changeRoom() {
-    var currentRoom = $( "#roomSelector option:selected" ).text();
-    //Display all if currentRoom === home
-    $(".chat").each(function(index, element) {
-      
-    });
-  }
+  $('.user').click(function() {
+    alert("clicked!");
+    friendsList[$(this).text()] = $(this).text();
+  });
 });
+function changeRoom() {
+  var selectedRoom = $( "#roomSelector option:selected" ).text();
+  //Display all if currentRoom === home
+  $(".chat").each(function(index, element) {
+    // debugger;
+    var targetRoom = displayedMessages[$(this).attr("id")].roomname;
+    if (selectedRoom === targetRoom || selectedRoom === "Home") {
+      $(this).css("display", "block");
+    }
+    else {
+      $(this).css("display", "none");
+    }
+  });
+}
